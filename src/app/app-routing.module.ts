f7d4800a64baf26ea08529ca0474a1877c6f3b7e
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-07-15 12:25:54 +0200

    reordering foldes and correcting footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { StampComponent } from "./stamp/stamp.component";
-import { JewelsComponent } from "./jewels/jewels.component";
-import { ContactComponent } from "./contact/contact.component";
-import { CollectionsComponent } from "./collections/collections.component";
+import { HomeComponent } from "./views/home/home.component";
+import { StampComponent } from "./views/stamp/stamp.component";
+import { JewelsComponent } from "./views/jewels/jewels.component";
+import { ContactComponent } from "./views/contact/contact.component";
+import { CollectionsComponent } from "./views/collections/collections.component";
 import { CookiesComponent } from './views/cookies/cookies.component';
 import { CreditsComponent } from './views/credits/credits.component';
 import { LegalAdvertisementComponent } from './views/legal-advertisement/legal-advertisement.component';
